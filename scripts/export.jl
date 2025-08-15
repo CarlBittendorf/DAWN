82efdacfc8327fb634e16257d4fe1d882075414a
Carl Bittendorf <carl.bittendorf@gmail.com>
2025-08-15 13:36:27 +0200

    Update export script

--- scripts/export.jl
+++ scripts/export.jl
@@ -1,16 +1,11 @@
 include("../src/main.jl")
 
-df = DataFrame()
-
 for sc in STUDY_CENTERS
     city, username, password, clientsecret, studyuuid = sc.name,
     sc.username, sc.password, sc.client_secret, sc.studyuuid
 
-    df = vcat(
-        df,
-        download_interaction_designer_dataframe(
-            username, password, clientsecret, studyuuid)
-    )
-end
+    df = download_interaction_designer_dataframe(
+        username, password, clientsecret, studyuuid)
 
-CSV.write("export/CRC393 Forms (raw).csv", df)
+    CSV.write("export/CRC393 $city Forms (raw).csv", df)
+end
