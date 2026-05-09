1e7d6212983f38c76a67b465d6059b167b07859b
Carl Bittendorf <carl.bittendorf@gmail.com>
2026-05-09 12:07:51 +0200

    Update compliance script

--- scripts/compliance.jl
+++ scripts/compliance.jl
@@ -1,18 +1,23 @@
 include("../src/main.jl")
 
 function script()
+    # read the study center index from command-line arguments
     index = parse(Int, ARGS[1])
-    sc = STUDY_CENTERS[index]
 
-    city = sc.name
+    # select the study center based on the provided index
+    study_center = STUDY_CENTERS[index]
 
+    # extract the city name from the study center metadata
+    city = study_center.name
+
+    # define a cutoff date (7 days ago) for compliance
     cutoff = Date(now()) - Day(7)
 
     # connection to database
     db = DuckDB.DB(joinpath("data", city * ".db"))
 
     # contains :Participant, :MovisensXSParticipantID, :Instance and :AssignmentDate columns
-    df_movisensxs = read_dataframe(db, "movisensxs")
+    df_movisensxs = read_database(DatabaseMovisensXS, db)
 
     df_sensing = DataFrame(
         :Participant => get_mobile_sensing_participants(df_movisensxs),
@@ -21,7 +26,7 @@ function script()
 
     df_running = @chain begin
         # contains :Participant and :Date columns
-        read_dataframe(db, "running")
+        read_database(DatabaseSensingRunning, db)
 
         transform(All() => ByRow((x...) -> true) => :MobileSensingRunning)
     end
@@ -30,7 +35,7 @@ function script()
 
     df = @chain begin
         # contains :Participant, :DateTime, :Variable and :Value columns
-        read_dataframe(db, "queries")
+        read_database(DatabaseQueries, db)
 
         # remove test accounts
         subset(:Participant => ByRow(x -> !(x in TEST_ACCOUNTS)))
@@ -38,11 +43,7 @@ function script()
         subset(:Variable => ByRow(x -> x in ["ChronoRecord", "IsA04"]))
 
         # remove leading white space
-        transform(
-            :Participant => ByRow(x -> isnothing(tryparse(Int, x)) ? x :
-                                       lpad(parse(Int, x), 4, "0"));
-            renamecols = false
-        )
+        transform(:Participant => ByRow(lstrip); renamecols = false)
 
         # entries before 05:30 are considered to belong to the previous day
         transform(:DateTime => ByRow(x -> Time(x) <= Time("05:30") ? Date(x) - Day(1) : Date(x)) => :Date)
@@ -90,21 +91,22 @@ function script()
         )
 
         # add :InteractionDesignerParticipantUUID, :InteractionDesignerGroup and :StudyCenter columns
-        leftjoin(read_dataframe(db, "participants"); on = :Participant)
+        leftjoin(read_database(DatabaseParticipants, db); on = :Participant)
         dropmissing(:InteractionDesignerGroup)
 
-        # add :IsA06, :IsB01, :IsB03, :IsB05, :IsB07, :IsC01, :IsC02, :IsC03 and :IsC04 columns
-        leftjoin(read_dataframe(db, "subprojects"); on = :Participant)
+        # add :A06, :B01, :B03, :B05, :B07, :C01, :C02, :C03 and :C04 columns
+        leftjoin(read_database(DatabaseSubprojects, db); on = :Participant)
         transform(
-            [:IsA06, :IsB01, :IsB03, :IsB05, :IsB07, :IsC01, :IsC02, :IsC03, :IsC04] .=>
+            [:A06, :B01, :B03, :B05, :B07, :C01, :C02, :C03, :C04] .=>
                 ByRow(x -> !ismissing(x) && x);
             renamecols = false
         )
 
         subset(:InteractionDesignerGroup => ByRow(x -> !contains(x, "Partner")))
 
-        transform(:IsA04 => ByRow(x -> isvalid(x) ? x : false); renamecols = false)
-        transform("Is" .* projects => ByRow((x...) -> projects[[x...]]) => :Projects)
+        rename(:IsA04 => :A04)
+        transform(:A04 => ByRow(x -> isvalid(x) ? x : false); renamecols = false)
+        transform(projects => ByRow((x...) -> projects[[x...]]) => :Projects)
     end
 
     for (selection, email) in EMAIL_COMPLIANCE_TABLE[city]
