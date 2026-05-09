1be177e3565a479e6effc340d788bc3db81b950b
Carl Bittendorf <carl.bittendorf@gmail.com>
2026-05-09 12:17:56 +0200

    Improve quality script

--- scripts/quality.jl
+++ scripts/quality.jl
@@ -17,7 +17,11 @@ function script()
 
         select(:Participant, :Instance, :HAMD, :HAMDDate)
 
-        add_table!(tables, "Missing information on the inflection signal", _)
+        add_table!(
+            tables,
+            "Missing information about the inflection signal due to transmission errors",
+            _
+        )
     end
 
     @chain df_clarification begin
@@ -32,7 +36,109 @@ function script()
 
         select(:Participant, :Instance, :SignalDate)
 
-        add_table!(tables, "No information on whether the participant was reached", _)
+        add_table!(
+            tables,
+            "No information on whether the participant was reached, \
+            even though the inflection signal was sent over two weeks ago \
+            (please add this information)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        dropmissing(:CloseInstanceMania)
+        subset(
+            :InflectionSignalType => ByRow(isequal("InflectionDepression")),
+            :CloseInstanceMania => ByRow(!ismissing)
+        )
+
+        select(:Participant, :Instance, :CloseInstanceDepression, :CloseInstanceMania)
+
+        add_table!(
+            tables,
+            "Depressive inflection signal, but the instance-closing variable for mania is used \
+            (please use the variable for depression instead)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        dropmissing(:CloseInstanceDepression)
+        subset(
+            :InflectionSignalType => ByRow(isequal("InflectionMania")),
+            :CloseInstanceDepression => ByRow(!ismissing)
+        )
+
+        select(:Participant, :Instance, :CloseInstanceDepression, :CloseInstanceMania)
+
+        add_table!(
+            tables,
+            "Manic inflection signal, but the instance-closing variable for depression is used \
+            (please use the variable for mania instead)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        dropmissing(:CloseInstanceDepression)
+        subset(
+            :CloseInstanceDepression,
+            :CloseInstanceDepressionDate => ByRow(ismissing)
+        )
+
+        select(:Participant, :Instance)
+
+        add_table!(
+            tables,
+            "Date of instance closure missing (depressive inflection signals, please add)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        dropmissing(:CloseInstanceMania)
+        subset(
+            :CloseInstanceMania,
+            :CloseInstanceManiaDate => ByRow(ismissing)
+        )
+
+        select(:Participant, :Instance)
+
+        add_table!(
+            tables,
+            "Date of instance closure missing (manic inflection signals, please add)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        dropmissing(:CloseInstanceDepression)
+        subset(:CloseInstanceDepression => ByRow(!))
+
+        select(:Participant, :Instance, :CloseInstanceDepression, :CloseInstanceMania)
+
+        add_table!(
+            tables,
+            "The instance closure variable is set to false, \
+            but it should only be set to missing for open instances and to true for closed instances \
+            (depressive inflection signals, please correct this)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        dropmissing(:CloseInstanceMania)
+        subset(:CloseInstanceMania => ByRow(!))
+
+        select(:Participant, :Instance, :CloseInstanceDepression, :CloseInstanceMania)
+
+        add_table!(
+            tables,
+            "The instance closure variable is set to false, \
+            but it should only be set to missing for open instances and to true for closed instances \
+            (manic inflection signals, please correct this)",
+            _
+        )
     end
 
     @chain df_clarification begin
@@ -45,7 +151,28 @@ function script()
 
         select(:Participant, :Instance, :SignalDate, :TelephoneReached)
 
-        add_table!(tables, "Instances on consecutive days", _)
+        add_table!(
+            tables,
+            "Instances on consecutive days (please check if this is correct)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        transform([:InflectionDepressionSecondDate, :InflectionManiaSecondDate]
+        => ByRow((x...) -> coalesce(x...) + Day(1)) => :SignalDate)
+        dropmissing([:SignalDate, :TelephoneDate])
+        transform([:SignalDate, :TelephoneDate] => ByRow((s, t) -> t - s) => :Difference)
+        subset(:Difference => ByRow(x -> x < Day(0) || x > Week(2)))
+
+        select(:Participant, :Instance, :SignalDate, :TelephoneDate, :Difference)
+
+        add_table!(
+            tables,
+            "Unusual time differences between inflection signal and telephone date \
+            (please check if this is correct)",
+            _
+        )
     end
 
     @chain df_clarification begin
@@ -54,7 +181,12 @@ function script()
 
         select(:Participant, :Instance, :TelephoneReached, :HAMD, :HAMDDate)
 
-        add_table!(tables, "HAM-D without reaching the participant", _)
+        add_table!(
+            tables,
+            "HAM-D was conducted, but the participant was reportedly not reached \
+            (please add that the participant has been reached)",
+            _
+        )
     end
 
     @chain df_clarification begin
@@ -63,7 +195,12 @@ function script()
 
         select(:Participant, :Instance, :TelephoneReached, :YMRS, :YMRSDate)
 
-        add_table!(tables, "YMRS without reaching the participant", _)
+        add_table!(
+            tables,
+            "YMRS was conducted, but the participant was reportedly not reached \
+            (please add that the participant has been reached)",
+            _
+        )
     end
 
     @chain df_clarification begin
@@ -71,7 +208,7 @@ function script()
 
         select(:Participant, :Instance, :HAMD, :HAMDDate)
 
-        add_table!(tables, "HAM-D without date", _)
+        add_table!(tables, "HAM-D without date (please add)", _)
     end
 
     @chain df_clarification begin
@@ -79,7 +216,7 @@ function script()
 
         select(:Participant, :Instance, :YMRS, :YMRSDate)
 
-        add_table!(tables, "YMRS without date", _)
+        add_table!(tables, "YMRS without date (please add)", _)
     end
 
     @chain df_clarification begin
@@ -88,7 +225,7 @@ function script()
 
         select(:Participant, :Instance, :HAMD, :HAMDDate)
 
-        add_table!(tables, "HAM-D without interviewer", _)
+        add_table!(tables, "HAM-D without interviewer (please add)", _)
     end
 
     @chain df_clarification begin
@@ -97,33 +234,65 @@ function script()
 
         select(:Participant, :Instance, :YMRS, :YMRSDate)
 
-        add_table!(tables, "YMRS without interviewer", _)
+        add_table!(tables, "YMRS without interviewer (please add)", _)
     end
 
     @chain df_clarification begin
-        dropmissing([:TelephoneDate, :HAMDDate, :YMRSDate])
-        subset([:TelephoneDate, :HAMDDate, :YMRSDate] => ByRow((t, h, y) -> t != h || t != y))
-        sort(:TelephoneDate)
+        subset(
+            :InflectionSignalType => ByRow(isequal("InflectionDepression")),
+            :TelephoneReached => ByRow(x -> !ismissing(x) || x),
+            [:TelephoneDate, :HAMDDate] => ByRow(!isequal)
+        )
 
-        select(:Participant, :Instance, :TelephoneDate, :HAMDDate, :YMRSDate)
+        select(:Participant, :Instance, :TelephoneDate, :HAMDDate)
 
         add_table!(
-            tables, "Contradictory information regarding the date of the telephone interview", _)
+            tables,
+            "Contradictory information regarding the date of the telephone interview \
+            (depressive inflection signals, please ensure that both variables contain the correct date)",
+            _
+        )
     end
 
     @chain df_clarification begin
-        subset([:DepressiveEpisode, :ManicEpisode] => ByRow((x...) -> any(!ismissing, x)))
+        subset(
+            :InflectionSignalType => ByRow(isequal("InflectionMania")),
+            :TelephoneReached => ByRow(x -> !ismissing(x) || x),
+            [:TelephoneDate, :YMRSDate] => ByRow(!isequal)
+        )
+
+        select(:Participant, :Instance, :TelephoneDate, :YMRSDate)
+
+        add_table!(
+            tables,
+            "Contradictory information regarding the date of the telephone interview \
+            (manic inflection signals, please ensure that both variables contain the correct date)",
+            _
+        )
+    end
+
+    @chain df_clarification begin
+        subset(
+            :InflectionSignalType => ByRow(isequal("InflectionDepression")),
+            [:DepressiveEpisode, :ManicEpisode] => ByRow((x...) -> any(!ismissing, x))
+        )
         transform([:HAMDDate, :DIPSDate] => ByRow((h, d) -> d - h) => :Difference)
         subset(:Difference => ByRow(x -> x < Week(2) || x > Week(6)))
 
         select(:Participant, :Instance, :HAMDDate, :DIPSDate, :Difference)
 
         add_table!(
-            tables, "Unusual time differences between HAM-D date and DIPS", _)
+            tables,
+            "Unusual time differences between HAM-D date and DIPS (please check if this is correct)",
+            _
+        )
     end
 
     @chain df_clarification begin
-        subset([:DepressiveEpisode, :ManicEpisode] => ByRow((x...) -> any(!ismissing, x)))
+        subset(
+            :InflectionSignalType => ByRow(isequal("InflectionMania")),
+            [:DepressiveEpisode, :ManicEpisode] => ByRow((x...) -> any(!ismissing, x))
+        )
         dropmissing(:YMRSDate)
         transform([:YMRSDate, :DIPSDate] => ByRow((h, d) -> d - h) => :Difference)
         subset(:Difference => ByRow(x -> x < Week(2) || x > Week(6)))
@@ -131,7 +300,10 @@ function script()
         select(:Participant, :Instance, :YMRSDate, :DIPSDate, :Difference)
 
         add_table!(
-            tables, "Unusual time differences between YMRS date and DIPS", _)
+            tables,
+            "Unusual time differences between YMRS date and DIPS (please check if this is correct)",
+            _
+        )
     end
 
     @chain df_clarification begin
@@ -139,13 +311,19 @@ function script()
         subset(
             :HAMD => ByRow(x -> x > 8),
             [:DepressiveEpisode, :ManicEpisode] => ByRow((x...) -> all(ismissing, x)),
-            :HAMDDate => ByRow(x -> x < Date(now()) - Week(4))
+            :HAMDDate => ByRow(x -> x < Date(now()) - Week(4)),
+            :DIPSReached => ByRow(x -> ismissing(x) || x)
         )
         sort(:HAMDDate)
 
         select(:Participant, :Instance, :HAMD, :HAMDDate, :DIPSReached)
 
-        add_table!(tables, "Critical HAM-D without DIPS", _)
+        add_table!(
+            tables,
+            "Critical HAM-D without DIPS \
+            (please conduct the DIPS or mark as not reached for the on-site interview)",
+            _
+        )
     end
 
     @chain df_clarification begin
@@ -153,13 +331,19 @@ function script()
         subset(
             :YMRS => ByRow(x -> x > 7),
             [:DepressiveEpisode, :ManicEpisode] => ByRow((x...) -> all(ismissing, x)),
-            :YMRSDate => ByRow(x -> x < Date(now()) - Week(4))
+            :YMRSDate => ByRow(x -> x < Date(now()) - Week(4)),
+            :DIPSReached => ByRow(x -> ismissing(x) || x)
         )
         sort(:YMRSDate)
 
         select(:Participant, :Instance, :YMRS, :YMRSDate, :DIPSReached)
 
-        add_table!(tables, "Critical YMRS without DIPS", _)
+        add_table!(
+            tables,
+            "Critical YMRS without DIPS \
+            (please conduct the DIPS or mark as not reached for the on-site interview)",
+            _
+        )
     end
 
     html = make_html(
